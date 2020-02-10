41b226ee75a93dd0302a9e617365e69409e98e90
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-02-11 00:20:31 +0800

    feat: calculate mean median and mode of integer vector

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod integer_vector;
+
+fn main() {
+    let int_vec =
+        integer_vector::integer_vector::RandomIntegerVector::build_random_integer_vector();
+
+    println!("The mean of integer array is {}", int_vec.get_mean());
+    println!("The median of integer array is {}", int_vec.get_median());
+    println!("The mode of integer array is {}", int_vec.get_mode());
+}
