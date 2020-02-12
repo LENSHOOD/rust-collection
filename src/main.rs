e4595a74feb3f44211e8c3bdf4e9ebdc7cff6fcc
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-02-12 22:54:41 +0800

    refactor: add use keyword to simplify call

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 mod integer_vector;
 mod pig_latin;
 
+use integer_vector::integer_vector::RandomIntegerVector;
+use pig_latin::pig_latin::PigLatin;
+
 fn main() {
     let int_vec =
-        integer_vector::integer_vector::RandomIntegerVector::build_random_integer_vector();
+        RandomIntegerVector::build_random_integer_vector();
 
     println!("The generated integer array is {:?}", int_vec.get_int_vec());
     println!("The mean of integer array is {}", int_vec.get_mean());
     println!("The median of integer array is {}", int_vec.get_median());
     println!("The mode of integer array is {}", int_vec.get_mode());
 
-    let consonant = pig_latin::pig_latin::PigLatin::new("first");
-    let vowel = pig_latin::pig_latin::PigLatin::new("apple");
+    let consonant = PigLatin::new("first");
+    let vowel = PigLatin::new("apple");
 
     println!("The pig latin of consonant \"first\" is: {}", consonant.to_pig_latin());
     println!("The pig latin of consonant \"apple\" is: {}", vowel.to_pig_latin());
