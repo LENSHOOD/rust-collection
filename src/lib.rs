42d4a4baa15a8d209733568c2edc22733defe779
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-03-08 16:50:57 +0800

    test: add canary tests

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod canary_test {
+    #[test]
+    fn should_equal() {
+        assert_eq!(1 + 1, 2);
+    }
+
+    #[test]
+    fn should_not_equal() {
+        assert_ne!(1, 2);
+    }
+
+    #[test]
+    fn should_true() {
+        assert!(true);
+    }
+
+    #[test]
+    #[should_panic(expected = "Should Panic")]
+    fn should_panic() {
+        panic!("Should Panic!")
+    }
+}
